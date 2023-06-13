d896f8351344b5acd5c8b375ffecc0a964868f7b
cypl <cyrille.plenchette@me.com>
2023-06-13 12:11:49 +0200

    add function copy clipboard

--- src/components/modal-contents/Export.jsx
+++ src/components/modal-contents/Export.jsx
@@ -17,6 +17,13 @@ function Export() {
   const [selectCopyHover, setSelectCopyHover] = useState(false)
   const [isCopied, setIsCopied] = useState(false)
 
+  const elementToCopy = document.querySelector('.copy-clipboard')
+  function copyColors(toCopy) {
+    setIsCopied(true)
+    const stringToCopy = toCopy.innerText
+    navigator.clipboard.writeText(stringToCopy)
+  }
+
   return (
     <>
       <WrapperExport>
@@ -27,63 +34,75 @@ function Export() {
         <SelectCopy
           onMouseEnter={() => setSelectCopyHover(true)}
           onMouseLeave={() => setSelectCopyHover(false)}
-          onClick={() => setIsCopied(true)}
+          onClick={() => copyColors(elementToCopy)}
         >
           <IconCopy>
-            {selectCopyHover && (
-              <p>
-                Cliquer <br />
-                pour copier
-              </p>
-            )}
+            {selectCopyHover &&
+              (!isCopied ? (
+                <p>
+                  Cliquer <br />
+                  pour copier
+                </p>
+              ) : (
+                <p>
+                  C&apos;est <br />
+                  copié !
+                </p>
+              ))}
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M272 0H396.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H272c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128H192v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z" />
             </svg>
           </IconCopy>
-          <p>
-            <ColorCircle
-              style={{ backgroundColor: colorPrimaire1 }}
-            ></ColorCircle>
-            <ColorName>$primaire1:</ColorName> {colorPrimaire1};
-          </p>
-          <p>
-            <ColorCircle
-              style={{ backgroundColor: colorPrimaire }}
-            ></ColorCircle>
-            <ColorName>$primaire:</ColorName> {colorPrimaire};
-          </p>
-          <p>
-            <ColorCircle
-              style={{ backgroundColor: colorPrimaire2 }}
-            ></ColorCircle>
-            <ColorName>$primaire2:</ColorName> {colorPrimaire2};
-          </p>
-          <p>
-            <ColorCircle
-              style={{ backgroundColor: colorSecondaire1 }}
-            ></ColorCircle>
-            <ColorName>$secondaire1:</ColorName> {colorSecondaire1};
-          </p>
-          <p>
-            <ColorCircle
-              style={{ backgroundColor: colorSecondaire }}
-            ></ColorCircle>
-            <ColorName>$secondaire:</ColorName> {colorSecondaire};
-          </p>
-          <p>
-            <ColorCircle
-              style={{ backgroundColor: colorSecondaire2 }}
-            ></ColorCircle>
-            <ColorName>$secondaire2:</ColorName> {colorSecondaire2};
-          </p>
-          <p>
-            <ColorCircle style={{ backgroundColor: colorClair }}></ColorCircle>
-            <ColorName>$clair:</ColorName> {colorClair};
-          </p>
-          <p>
-            <ColorCircle style={{ backgroundColor: colorClair2 }}></ColorCircle>
-            <ColorName>$clair2:</ColorName> {colorClair2};
-          </p>
+          <div className="copy-clipboard">
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorPrimaire1 }}
+              ></ColorCircle>
+              <ColorName>$primaire1:</ColorName> {colorPrimaire1};
+            </p>
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorPrimaire }}
+              ></ColorCircle>
+              <ColorName>$primaire:</ColorName> {colorPrimaire};
+            </p>
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorPrimaire2 }}
+              ></ColorCircle>
+              <ColorName>$primaire2:</ColorName> {colorPrimaire2};
+            </p>
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorSecondaire1 }}
+              ></ColorCircle>
+              <ColorName>$secondaire1:</ColorName> {colorSecondaire1};
+            </p>
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorSecondaire }}
+              ></ColorCircle>
+              <ColorName>$secondaire:</ColorName> {colorSecondaire};
+            </p>
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorSecondaire2 }}
+              ></ColorCircle>
+              <ColorName>$secondaire2:</ColorName> {colorSecondaire2};
+            </p>
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorClair }}
+              ></ColorCircle>
+              <ColorName>$clair:</ColorName> {colorClair};
+            </p>
+            <p>
+              <ColorCircle
+                style={{ backgroundColor: colorClair2 }}
+              ></ColorCircle>
+              <ColorName>$clair2:</ColorName> {colorClair2};
+            </p>
+          </div>
         </SelectCopy>
       </WrapperExport>
     </>
